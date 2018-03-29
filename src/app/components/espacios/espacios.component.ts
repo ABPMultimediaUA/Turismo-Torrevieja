feb792d27088693bd7c5b7f28a01f621d0a77e6e
riguer <isrem89@gmail.com>
2018-03-29 13:38:04 +0200

    Material en espacios, depurado

--- src/app/components/espacios/espacios.component.ts
+++ src/app/components/espacios/espacios.component.ts
@@ -1,94 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PeticionesCrudService, LogueadoService } from '../../services/index';
 import { EspacioInterface } from '../../interfaces/espacio.interface';
 import { Router } from '@angular/router';
 
-import { MatTableDataSource, MatSort } from '@angular/material';
-import { MatFormFieldModule } from '@angular/material';
-import { MatInputModule } from '@angular/material';
-import { SelectionModel } from '@angular/cdk/collections';
-import { MatPaginatorModule, MatPaginator } from '@angular/material';
-import { MatIcon } from '@angular/material';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconRegistry, MatIconModule, MatButtonModule } from '@angular/material';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+// import { MatFormFieldModule } from '@angular/material';
+// import {MatInputModule} from '@angular/material';
+// import {SelectionModel} from '@angular/cdk/collections';
+// import { MatPaginatorModule } from '@angular/material';
+// import { MatIcon} from '@angular/material';
+// import {MatCheckboxModule} from '@angular/material/checkbox';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatIconRegistry, MatIconModule, MatButtonModule } from '@angular/material';
+// import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+// import {MatDialogModule} from '@angular/material/dialog';
+// import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// import {EliminarUsuarioDialog} from "../usuarios/eliminar-usuario-dialog.component";
+// import {EditarUsuarioDialog} from "../usuarios/editar-usuario-dialog.component";
 
 @Component({
   selector: 'app-espacios',
   templateUrl: './espacios.component.html',
-  styleUrls: ['./espacios.component.css']
+  // styleUrls: ['./espacios.component.css'],
+  styleUrls: ['../../app.component.css']
 })
 
 export class EspaciosComponent implements OnInit {
 
-  espacio:EspacioInterface[]=[];
-  espaciosActuales:any[] = [];
-  totalPaginas:number;
-  currentPage:number = 1;
-  loading:boolean = true;
-  cantidadPagina:any[]=[];
+  items:EspacioInterface[]=[];
 
   row:EspacioInterface;
-  displayedColumns = ['identificador', 'select'];
+  displayedColumns = ['identificador', 'nombreEspacio', 'sitio', 'aforo', 'eliminar'];
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(  private _espacioService: PeticionesCrudService,
+  dataSource = new MatTableDataSource(this.items);
+    // selection = new SelectionModel<EspacioInterface>(true, []);
+  ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource(this.items);
+    // this.selection = new SelectionModel<EspacioInterface>(true, []);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  constructor(  private _itemService: PeticionesCrudService,
                 public  logueadoService: LogueadoService,
                 private router:Router
              )
   {
     this.logueadoService.comprobarLogueado();
 
-    this._espacioService.getItem(6,-1,-1,-1).then(
+    this._itemService.getItem(6,-1,-1,-1).then(
       res => {
-        this.espacio = res as EspacioInterface[];
-
-        this.totalPaginas = Math.ceil(this.espacio.length/10);
-        this.loading=false;
-
-        for(let i=0;i<this.totalPaginas;i++){ this.cantidadPagina.push(i); }
-
-        if(this.espacio.length>9){
-          for(let i=0;i<=9;i++){ this.espaciosActuales.push(this.espacio[i]); }
-        }
-        else{
-          for(let i=0;i<this.espacio.length;i++){ this.espaciosActuales.push(this.espacio[i]); }
-        }
+        this.items = res as EspacioInterface[];
+        this.ngAfterViewInit();
       }
     );
-
   }
 
   ngOnInit() {
-  }
-
-  nuevaPagina(pagina:number){
-    this.currentPage=pagina;
-    let x = 10 * (pagina-1);
-    let y = x + 9;
-    this.espaciosActuales=[];
-
-    if(pagina==this.totalPaginas){
-      for(let i=x;i<this.espacio.length;i++){
-        this.espaciosActuales.push(this.espacio[i]);
-      }
-    }else{
-      for(let i=x;i<=y;i++){
-        this.espaciosActuales.push(this.espacio[i]);
-      }
-    }
-  }
-
-  borrarItem(id:number){
-    this._espacioService.eliminarItem(6,id,-1)
-    .then( res =>{
-      if(res){
-        this._espacioService.getItem(6,1,-1,-1);
-        location.reload(true);
-        this.router.navigate(['espacios']);
-      }
-    })
   }
 
 }
