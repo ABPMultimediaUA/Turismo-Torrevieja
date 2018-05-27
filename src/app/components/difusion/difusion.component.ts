c2d00df52d505cc2279b955a86d91b4097774ab7
jrp60 <javirodenas.javirodenas@gmail.com>
2018-05-27 18:15:05 +0200

    Subiendo cambios

--- src/app/components/difusion/difusion.component.ts
+++ src/app/components/difusion/difusion.component.ts
@@ -31,143 +31,6 @@ id:string;
     nombreExpediente:"",
     titulo:"",
   };
-//
-//    Base64 = {
-// // private property
-// _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-//
-// // public method for encoding
-// encode : function (input2:string) {
-//     var output = "";
-//     var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-//     var i = 0;
-//
-//     var input = this.Base64._utf8_encode(input2);
-//
-//     while (i < input.length) {
-//
-//         chr1 = input.charCodeAt(i++);
-//         chr2 = input.charCodeAt(i++);
-//         chr3 = input.charCodeAt(i++);
-//
-//         enc1 = chr1 >> 2;
-//         enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-//         enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-//         enc4 = chr3 & 63;
-//
-//         if (isNaN(chr2)) {
-//             enc3 = enc4 = 64;
-//         } else if (isNaN(chr3)) {
-//             enc4 = 64;
-//         }
-//
-//         output = output +
-//         this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-//         this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-//
-//     }
-//
-//     return output;
-// },
-//
-// // public method for decoding
-// decode : function (input) {
-//     var output = "";
-//     var chr1, chr2, chr3;
-//     var enc1, enc2, enc3, enc4;
-//     var i = 0;
-//
-//     input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-//
-//     while (i < input.length) {
-//
-//         enc1 = this._keyStr.indexOf(input.charAt(i++));
-//         enc2 = this._keyStr.indexOf(input.charAt(i++));
-//         enc3 = this._keyStr.indexOf(input.charAt(i++));
-//         enc4 = this._keyStr.indexOf(input.charAt(i++));
-//
-//         chr1 = (enc1 << 2) | (enc2 >> 4);
-//         chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-//         chr3 = ((enc3 & 3) << 6) | enc4;
-//
-//         output = output + String.fromCharCode(chr1);
-//
-//         if (enc3 != 64) {
-//             output = output + String.fromCharCode(chr2);
-//         }
-//         if (enc4 != 64) {
-//             output = output + String.fromCharCode(chr3);
-//         }
-//
-//     }
-//
-//     output = this.Base64._utf8_decode(output);
-//
-//     return output;
-//
-// },
-//
-// // private method for UTF-8 encoding
-// _utf8_encode : function (string) {
-//     string = string.replace(/\r\n/g,"\n");
-//     var utftext = "";
-//
-//     for (var n = 0; n < string.length; n++) {
-//
-//         var c = string.charCodeAt(n);
-//
-//         if (c < 128) {
-//             utftext += String.fromCharCode(c);
-//         }
-//         else if((c > 127) && (c < 2048)) {
-//             utftext += String.fromCharCode((c >> 6) | 192);
-//             utftext += String.fromCharCode((c & 63) | 128);
-//         }
-//         else {
-//             utftext += String.fromCharCode((c >> 12) | 224);
-//             utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-//             utftext += String.fromCharCode((c & 63) | 128);
-//         }
-//
-//     }
-//
-//     return utftext;
-// },
-//
-// // private method for UTF-8 decoding
-// _utf8_decode : function (utftext) {
-//     var string = "";
-//     var i = 0;
-//     var c = 0;
-//     var c2 = 0;
-//     var c3 = 0;
-//
-//     while ( i < utftext.length ) {
-//
-//         c = utftext.charCodeAt(i);
-//
-//         if (c < 128) {
-//             string += String.fromCharCode(c);
-//             i++;
-//         }
-//         else if((c > 191) && (c < 224)) {
-//             c2 = utftext.charCodeAt(i+1);
-//             string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-//             i += 2;
-//         }
-//         else {
-//             c2 = utftext.charCodeAt(i+1);
-//             c3 = utftext.charCodeAt(i+2);
-//             string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-//             i += 3;
-//         }
-//
-//     }
-//
-//     return string;
-// }
-//
-// }
 
   public expediente2:ExpedienteInterface={
     identificador:0,
@@ -182,34 +45,28 @@ id:string;
     titulo:"",
   };
 
-  host:any = 'https://gvent.ovh/turismo-torrevieja/?rest_route=/wp/v2/posts/';
+
+  //host:any = 'https://gvent.ovh/turismo-torrevieja/?rest_route=/wp/v2/posts';
+  host:any = 'http://127.0.0.1:8080/x3/?rest_route=/wp/v2/posts';
   data:any[] = [];
 
   header = new Headers ({
     'Content-Type':'application/json',
-    //'Content-Length':222222,
-  //  'Authorization':'Basic ' + btoa('wpTorrevieja:ar@&78Gl7)^EIR3jk*LHqtuU'),
-  //  'Authorization': 'Basic ' + this.Base64.encode('wpTorrevieja:ar@&78Gl7)^EIR3jk*LHqtuU')
-    'Authorization': 'Basic d3BUb3JyZXZpZWphOmFyQCY3OEdsNyleRUlSM2prKkxIcXR1VQ==',
-    'X-RequestDigest':'requestDigest',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'ET, POST, PATCH, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Authorization, Content-Type'
+  //  'Authorization': 'Basic d3BUb3JyZXZpZWphOmFyQCY3OEdsNyleRUlSM2prKkxIcXR1VQ==',
+    'Authorization': 'Basic amF2aXgzOkVheHlXSChIWERsJnJ0WG03T0tRZFRNNA==',
+    // 'X-RequestDigest':'requestDigest',
+     'Access-Control-Allow-Origin': '*',
+     'Access-Control-Allow-Methods': 'ET, POST, PATCH, PUT, DELETE, OPTIONS',
+     'Access-Control-Allow-Credentials':'true',
+  //   'Access-Control-Allow-Headers': 'Authorization, Content-Type,accept, origin, X-Requested-With, X-Authentication',
+     'Access-Control-Allow-Headers': '*',
+     'Access-Control-Expose-Headers': 'X-WP-Total, X-WP-TotalPages',
+  //   'Access-Control-Max-Age': '1728000'
 
   });
 
 
-//habilitar esta ruta para probar si carga el evento y tomar los datos, luego enviar a la de turismo torrevieja
- //https://gvent.ovh/Prueba2_1/public/expediente/1
-
-//https://gvent.ovh/turismo-torrevieja/wp-json/wp/v2/posts
-//https://gvent.ovh/turismo-torrevieja/index.php/wp-json/wp/v2/posts
-
-
-
 //https://gvent.ovh/turismo-torrevieja/?rest_route=/wp/v2/posts/1
-
-//RUTA BUENA
 
   constructor(private http:Http,
                 private _ItemService: PeticionesCrudService,
@@ -222,19 +79,21 @@ id:string;
                         this._ItemService.getItem(0,this.id,-1,-1).then(res => {
                             if(typeof res != "string"){
                               let r = res as any;
-                              this.expediente = r.data as ExpedienteInterface;
+                              this.expediente2 = r.data as ExpedienteInterface;
 
                             //  this.data[0]=this.expediente.titulo;
                             //  this.data[1]=this.expediente.detalle;
 
                             }
                             //cargamos imagen
-                            if(this.expediente.image){
-                              this.expediente.image = "https://gvent.ovh/Prueba2_1/public/img/" + this.expediente.image;
+                            if(this.expediente2.image){
+                              this.expediente2.image = "https://gvent.ovh/Prueba2_1/public/img/" + this.expediente2.image;
                               let o = this.etiqueta.nativeElement as HTMLImageElement;
-                              o.src = this.expediente.image;
+                              o.src = this.expediente2.image;
                             }
-                            this.coordinador = +this.expediente.coordinador;
+                            this.coordinador = +this.expediente2.coordinador;
+
+                            this.expediente=this.expediente2;
 
                           });
                 console.log("WOEWOEWOWE");
@@ -253,19 +112,21 @@ id:string;
   enviarPostWP(){
     let promise = new Promise((resolve, reject) => {
       let url = this.host;
-
+      console.log("1");
     //let body = JSON.stringify( this.expediente );
       let headers = this.header;
       let bodyAux={
-        titulo:this.expediente.titulo,
-        detalle:this.expediente.detalle
+        title:this.expediente.titulo,
+        content:this.expediente.detalle
       }
+      console.log("2 "+headers);
 
-      this.http.post(url, bodyAux, { headers })
+      this.http.post(url, bodyAux , { headers: headers })
         .toPromise()
           .then(  (res) => { resolve( res.json().data ); },
                   (err) => { resolve( err.toString() )}
           )
+      console.log("3");
           // .catch((err) => { console.log(err.toString()); console.error(err); })
     });
     return promise;
